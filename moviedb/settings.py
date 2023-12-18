35a36dd7f0852156203385c19e59e60104e8dcda
ChaitanyaSimhadri <98271951+ChaitanyaSimhadri@users.noreply.github.com>
2023-12-18 10:57:45 +0530

    updating settings

--- moviedb/settings.py
+++ moviedb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^eg1b@5uig-d(gk5r)tubg(@!t&xn^xh%@5pw)w&5i7=x-0c*@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
